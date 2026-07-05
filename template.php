1866c0d35350de325db215e5b51ff1512ee33447
Muh. Yasir <macbook@MacBook-Pro-macbook.local>
2026-07-05 22:08:39 +0900

    fix font tab

--- template.php
+++ template.php
@@ -29,7 +29,7 @@ $user = current_user();
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/css/select2.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css">
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css?v=<?= APP_VERSION ?>">
+  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css?v=<?= filemtime(__DIR__ . '/assets/css/style.css') ?>">
 </head>
 
 <body>
@@ -248,7 +248,7 @@ $user = current_user();
     }
   </script>
   <!-- Custom JS -->
-  <script src="<?= BASE_URL ?>assets/js/app.js?v=<?= APP_VERSION ?>"></script>
+  <script src="<?= BASE_URL ?>assets/js/app.js?v=<?= filemtime(__DIR__ . '/assets/js/app.js') ?>"></script>
   <!-- Extra JS per-halaman (dirender SETELAH jQuery siap) -->
   <?= $extra_js ?? '' ?>
 </body>
